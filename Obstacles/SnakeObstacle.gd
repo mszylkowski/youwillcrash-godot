ba9b0d4051d061921a89d55fbfe323b2669317b6
Matias Szylkowski <mszylkowski@hotmail.com>
2025-05-23 17:25:52 -0400

    pop animation for snake movement

--- Obstacles/SnakeObstacle.gd
+++ Obstacles/SnakeObstacle.gd
@@ -1,56 +1,57 @@
 extends Node2D
 
-@export var speed := 200.0
-@export var offset_x := 250.
-@export var offset_y := 400.
+@export_range(0.01, 1., .01) var move_time := .15 ## How often the snake moves forward
+@export var offset_x := 250
+@export var offset_y := 400
 @export var dist_between_parts := 40.0
 
 const SNAKE_PART := preload("res://Obstacles/SnakePart.tscn") as PackedScene
 
 func _ready() -> void:
 	var angle = randi_range(0, 3)
-	var lin_vel := -Vector2.from_angle(angle * PI * .5) * speed
+	var lin_vel := -Vector2.from_angle(angle * PI * .5) * dist_between_parts
 
-	var glob_pos := Vector2.ZERO
+	var start_pos := Vector2.ZERO
 	var rand_val := randf_range(-1, 1)
 	if angle == 0:
-		glob_pos = Vector2(offset_x * 1.5, offset_y * rand_val)
-		lin_vel = Vector2.LEFT * speed
+		start_pos = Vector2(offset_x * 1.5, offset_y * rand_val)
+		lin_vel = Vector2.LEFT * dist_between_parts
 	elif angle == 1:
-		glob_pos = Vector2(rand_val * offset_x, offset_y * 1.5)
-		lin_vel = Vector2.UP * speed
+		start_pos = Vector2(rand_val * offset_x, offset_y * 1.5)
+		lin_vel = Vector2.UP * dist_between_parts
 	elif angle == 2:
-		glob_pos = Vector2(-offset_x * 1.5, offset_y * rand_val)
-		lin_vel = Vector2.RIGHT * speed
+		start_pos = Vector2(-offset_x * 1.5, offset_y * rand_val)
+		lin_vel = Vector2.RIGHT * dist_between_parts
 	else:
-		glob_pos = Vector2(rand_val * offset_x, -offset_y * 1.5)
-		lin_vel = Vector2.DOWN * speed
+		start_pos = Vector2(rand_val * offset_x, -offset_y * 1.5)
+		lin_vel = Vector2.DOWN * dist_between_parts
 
-	var turn_time := (randf_range(1.3, 2)) * (offset_y if angle % 2 else offset_x) / speed
+	var turn_move_max := (offset_x if angle % 2 else offset_y) * 2 / dist_between_parts
+	var turn_move := randi_range(roundi(turn_move_max * .3), roundi(turn_move_max * .7))
 	var next_vel := Vector2(-lin_vel.y, lin_vel.x) * (1 if randf() > .5 else -1)
+	var length := randi_range(5, 8)
 
-	for i in range(7):
-		var part := SNAKE_PART.instantiate()
+	var curr_pos := start_pos
+
+	for i in range(length - 1, -1, -1):
+		var part := SNAKE_PART.instantiate() as Node2D
+		curr_pos = start_pos - lin_vel.normalized() * i * dist_between_parts
+		part.global_position = curr_pos
 		add_child(part)
-		if i == 0:
-			part.global_position = glob_pos
-		else:
-			var last_part := get_child(i - 1)
-			part.global_position = last_part.position - lin_vel.normalized() * dist_between_parts
-		part.linear_velocity = lin_vel
-		part.get_tree().create_timer(turn_time).timeout.connect(rotate_part.bind(i, next_vel))
-		await get_tree().create_timer(dist_between_parts / speed).timeout
 
-	var destroy_time := maxf(offset_x, offset_y) * 5. / speed
-	await get_tree().create_timer(destroy_time).timeout
+	for i in range(turn_move + 20):
+		curr_pos += lin_vel if (i < turn_move) else next_vel
+		var part := get_child(0) as Node2D
+		part.global_position = curr_pos
+		scale_up(part)
+		move_child(part, get_child_count() - 1)
+		await get_tree().create_timer(move_time).timeout
+
 	queue_free()
 
-func rotate_part(i: int, next_vel: Vector2) -> void:
-	var part := get_child(i) as RigidBody2D
-	part.linear_velocity = next_vel
-	#if i > 0:
-		#var first_part := get_child(0) as RigidBody2D
-		#get_tree().physics_frame.connect(func():
-			#print("updating")
-			#part.global_position = first_part.global_position - next_vel.normalized() * dist_between_parts * i
-		#, ConnectFlags.CONNECT_ONE_SHOT)
+func scale_up(part: Node2D) -> void:
+	var tween := part.create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SPRING)
+	var part_sprite := part.get_child(0) as Node2D
+	var part_scale := part_sprite.scale
+	part_sprite.scale = Vector2.ZERO
+	tween.tween_property(part_sprite, "scale", part_scale, move_time)
