bed17360c3a1e9c12f44ca1b96ba37203e79ba59
Matias Szylkowski <mszylkowski@hotmail.com>
2025-06-16 00:52:15 -0400

    added progress bar per level

--- Scripts/UI/LevelLabel.gd
+++ Scripts/UI/LevelLabel.gd
@@ -1,11 +1,13 @@
 class_name LevelLabel extends Label
 
 var tween: Tween
+@onready var progress := $Progress as TextureProgressBar
 
 func _ready() -> void:
 	Events.died.connect(hide_label)
 	Events.intro_finished.connect(show_label)
 	Events.level_changed.connect(level_changed)
+	Events.score_changed.connect(score_changed)
 	modulate.a = 0
 
 func hide_label() -> void:
@@ -24,3 +26,6 @@ func level_changed(level: int) -> void:
 	if tween and tween.is_running():
 		return
 	tween = Animations.pop(self)
+
+func score_changed(score: int) -> void:
+	progress.value = score % 150
