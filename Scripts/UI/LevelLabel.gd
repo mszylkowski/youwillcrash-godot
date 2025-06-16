fea19668f84c5e6563944ee012dd77941e4e57b9
Matias Szylkowski <mszylkowski@hotmail.com>
2025-06-16 01:26:15 -0400

    added boss progress bar

--- Scripts/UI/LevelLabel.gd
+++ Scripts/UI/LevelLabel.gd
@@ -21,11 +21,18 @@ func show_label() -> void:
 	tween = Animations.animate_in(self)
 
 func level_changed(level: int) -> void:
-	if level <= 0: return
+	var progress_children := $BossProgress.get_children()
+	if level <= 0:
+		for i in range(progress_children.size()):
+			progress_children[i].value = 0
+		return
 	text = str(level)
 	if tween and tween.is_running():
 		return
 	tween = Animations.pop(self)
+
+	for i in range(progress_children.size()):
+		progress_children[i].value = 0 if (((level - 1) % 5 + 1) <= i) else 1
 
 func score_changed(score: int) -> void:
 	progress.value = score % 150
